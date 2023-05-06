c6299b4e4c0a3dbeecdf03fcd5418f5ea67deaa3
carmtsang <carmtsang89@hotmail.com>
2023-05-06 09:54:24 -0700

    add delete

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -46,7 +46,7 @@ def create():
 
 
 def get_post(id, check_author=True):
-    post = get_db.execute(
+    post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
@@ -88,3 +88,13 @@ def update(id):
             return redirect(url_for('blog.index'))
 
     return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
